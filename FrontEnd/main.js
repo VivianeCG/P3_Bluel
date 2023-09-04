4fc728e605bf8f4dd0003bd68cb473f935d23541
VivianeCG <chenet.viviane@gmail.com>
2023-09-04 23:40:06 +0200

    tentative incomplète pour créer les balises figure, img et figcaption

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -10,16 +10,52 @@ fetch ("http://localhost:5678/api/works")
     console.log (data)
 });
 
+//récupération des données depuis api
+const reponse =  fetch ("http://localhost:5678/api/works");
+const travaux =  reponse.json();
+
+
+//en théorie code (ou portion de code) pour créer les balises
+const conteneurPhoto = document.createElement("figure");
+const imageGallerie = document.createElement("img");
+const legendePhoto = document.createElement("figcaption");
+
+//en théorie devrait servir à permettre d'afficher les nouvelles div figure
+let conteneurParent = document.querySelector(".gallery");
+console.log()
+
+
 /**base = boucle WHILE vu qu'on peut ajouter des travaux plus tard
  * let i = 0
  * while (i<bidule.length) {
  * console.log (i)
  * i++}
- *  pour créer les <figure> avec img avec src et alt
- * function creationBaliseFigure (baliseImg, baliseFigcaption) {
- * return la balise créée
- * }
- */
+ * 
+ * let parentFigure = document.querySelector("div", ".gallery");
+    parentFigure.createElement("figure");
+    console.log("figure");
+ * 
+    code récup dans documentation avec qqs modifs qui ne marchent pas (erreur = undefined ou alors pas une fonction)
+    document.body.onload = addElement;
+
+document.section.onload = addElement;
+
+function addElement() {
+  // create a new div element
+  const baliseFigure = document.createElement("figure");
+
+  // and give it some content
+  const contenuTest = document.createTextNode("future balise image");
+
+  // add the text node to the newly created div
+   baliseFigure.appendChild(contenuTest);
+
+  // add the newly created element and its content into the DOM
+  const divOrigine = document.querySelector(".gallery");
+  document.section.insertAfter(baliseFigure, divOrigine);
+}
+*/
+
 
 /**pour vérifier qu'on peut récupérer les catégories */
 fetch ("http://localhost:5678/api/categories")

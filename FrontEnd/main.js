f61cf4b162f80325250a465cfcfe563a2b62008d
VivianeCG <chenet.viviane@gmail.com>
2023-09-07 11:28:54 +0200

    images (+leurs légendes) récupérées depuis le backend, HTML correspondant supprimé

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,67 +1,40 @@
-//pour lier le fichier à l'API
-const url ="http://localhost:5678/api-docs/";
-
-/**pour vérifier qu'on peut récupérer les travaux */
-fetch ("http://localhost:5678/api/works")
+const url = "http://localhost:5678/api/works"
+/**pour récupérer les travaux */
+fetch (url)
 .then (function (response){
     return response.json()
 })
-.then (function (data){
-    console.log (data)
+.then (function (data){//pour créer les balises figures dans le html
+    let conteneurParent = document.getElementById("gallery");
+    let conteneurPhoto;
+    let imageGallerie;
+    let legendePhoto;
+    data.forEach(element => {
+        conteneurPhoto = document.createElement("figure");
+        imageGallerie = document.createElement("img");
+        conteneurParent.appendChild(conteneurPhoto);
+        legendePhoto = document.createElement("figcaption");
+        conteneurPhoto.appendChild(imageGallerie);
+        conteneurPhoto.appendChild(legendePhoto);
+        imageGallerie.setAttribute("src","");
+        imageGallerie.src = element.imageUrl;
+        imageGallerie.setAttribute("alt","");
+        imageGallerie.alt = element.title;
+        legendePhoto.innerHTML = element.title; 
+        //console.log (data)
+        console.log (element);
+    });
 });
 
-//récupération des données depuis api
-const reponse =  fetch ("http://localhost:5678/api/works");
-const travaux =  reponse.json();
-
-
-//en théorie code (ou portion de code) pour créer les balises
-const conteneurPhoto = document.createElement("figure");
-const imageGallerie = document.createElement("img");
-const legendePhoto = document.createElement("figcaption");
-
-//en théorie devrait servir à permettre d'afficher les nouvelles div figure
-let conteneurParent = document.querySelector(".gallery");
-console.log()
-
-
-/**base = boucle WHILE vu qu'on peut ajouter des travaux plus tard
- * let i = 0
- * while (i<bidule.length) {
- * console.log (i)
- * i++}
- * 
- * let parentFigure = document.querySelector("div", ".gallery");
-    parentFigure.createElement("figure");
-    console.log("figure");
- * 
-    code récup dans documentation avec qqs modifs qui ne marchent pas (erreur = undefined ou alors pas une fonction)
-    document.body.onload = addElement;
-
-document.section.onload = addElement;
-
-function addElement() {
-  // create a new div element
-  const baliseFigure = document.createElement("figure");
-
-  // and give it some content
-  const contenuTest = document.createTextNode("future balise image");
-
-  // add the text node to the newly created div
-   baliseFigure.appendChild(contenuTest);
-
-  // add the newly created element and its content into the DOM
-  const divOrigine = document.querySelector(".gallery");
-  document.section.insertAfter(baliseFigure, divOrigine);
-}
+/**
 */
 
 
 /**pour vérifier qu'on peut récupérer les catégories */
-fetch ("http://localhost:5678/api/categories")
+/*fetch ("http://localhost:5678/api/categories")
 .then (function (response){
     return response.json()
 })
 .then (function (data){
     console.log (data)
-});
+});*/

cbd67d3b4b28f8a9b3b47d0b997b2e699cff1449
VivianeCG <chenet.viviane@gmail.com>
2023-09-09 13:33:06 +0200

    div filtres et div catégories créées

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,10 +1,20 @@
 const url = "http://localhost:5678/api/works"
-/**pour récupérer les travaux */
+/**pour récupérer les travaux depuis le Backend*/
 fetch (url)
 .then (function (response){
     return response.json()
 })
 .then (function (data){//pour créer les balises figures dans le html
+            {
+    let divConteneurCategories =document.getElementById("filtres");
+        let divFiltre;
+            for (let index = 0; index < 4; index++) {
+                divFiltre = document.createElement("div");
+                divFiltre.setAttribute('class', 'categorie');
+                divConteneurCategories.appendChild(divFiltre);
+                console.log(divFiltre);
+            };
+        }
     let conteneurParent = document.getElementById("gallery");
     let conteneurPhoto;
     let imageGallerie;
@@ -25,16 +35,59 @@ fetch (url)
         console.log (element);
     });
 });
+//creation des catégories de filtres
+/*let conteneurParentCategories;
+let conteneurPhoto=document.getElementById("gallery");
+window.onload=function (){
+    conteneurParentCategories = document.getElementById("portfolio"); //je veux le mettre dans le portfolio sous le h2
+    let divConteneurCategories = document.createElement("div");
+    conteneurParentCategories.appendChild(divConteneurCategories);
+    divConteneurCategories.setAttribute("id", "filtres");//éléments de class attribués à la nouvelle div
+    console.log(divConteneurCategories);
+    
+    function contenuFiltres(data) {
+        data.forEach(element=>{
+            divFiltre.innerHTML=element.categoryId;
+            console.log(divFiltre);
+        })
+        return contenuFiltres;
+    }
 
-/**
-*/
+}*/
+
+/*let conteneurParent = document.getElementById("gallery");
+let divConteneurCategories;
+window.onload=function(){
+divConteneurCategories.insertBefore(conteneurPhoto);
+divConteneurCategories=document.getElementById("filtres");
+}*/
 
 
-/**pour vérifier qu'on peut récupérer les catégories */
+/*let conteneurParentCategories = document.querySelector("#portfolio"); //je veux le mettre dans le portfolio sous le h2
+let divConteneurCategories = document.createElement("div");
+conteneurParentCategories.appendChild(divConteneurCategories);
+divConteneurCategories.setAttribute("id", "filtres");//éléments de class attribués à la nouvelle div
+console.log (divConteneurCategories);*/
+    //console.log(divConteneurCategories);
+
+//faudra peut-être créer la catégorie "tous" avant de faire la boucle pour les autres
 /*fetch ("http://localhost:5678/api/categories")
 .then (function (response){
     return response.json()
+
 })
-.then (function (data){
-    console.log (data)
-});*/
+.then (function creationZoneFiltres(data) {
+    let conteneurParentCategories = document.getElementById(portfolio); //je veux le mettre dans le portfolio sous le h2
+    let divConteneurCategories = document.createElement("div");
+    conteneurParentCategories.appendChild(divConteneurCategories);
+    divConteneurCategories.setAttribute("id", "filtres");//éléments de class attribués à la nouvelle div
+    let divFiltre;
+    data.for (let 'category' = 1; category < 4; category++;) {
+            divFiltre = document.createElement("div");
+            divFiltre.setAttribute('class', 'categorie');
+            divFiltre.innerText= "categories"
+            divConteneurCategories.appendChild(divFiltre);
+            console.log(divFiltre);
+        };
+});
+*/

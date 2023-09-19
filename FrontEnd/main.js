83c4fd944d3eda53e454bf3a8567a1992f8fee48
VivianeCG <chenet.viviane@gmail.com>
2023-09-19 22:15:45 +0200

    première idée de filtrage

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -34,11 +34,14 @@ function creationFiltres() {
         return response.json()
     })
     .then (function (data) {
+        let numeroId = 1;
         data.forEach(element=> {
             //pour créer les boutons des filtres avec leurs noms (sauf "Tous")
             let divConteneurCategories = document.getElementById("filtres");
             let button = document.createElement("button");
             button.setAttribute('class', 'categorie');
+            button.setAttribute('id', 'categorie'+numeroId);
+            numeroId++;
             divConteneurCategories.appendChild(button);
             button.innerHTML = element.name;
             console.log(button);
@@ -48,9 +51,27 @@ function creationFiltres() {
 }
 creationFiltres()
 
+function filtrageDesPhotos() {
+    const urlCategories = "http://localhost:5678/api/categories"
+    fetch (urlCategories)
+    .then (function (response){
+        return response.json()
+    })
+    .then (function (data) {
+        const categorieId = categorieId.set("#categorie"+numeroId => categorieId.id);
+        if (categorieId=categorieId.id) { //si l'id du bouton = l'id de la photo on l'affiche
+            console.log(data.id)
+            
+        } else {                // sinon on affiche tout
+            console.log()
+        }
+
+    });
+};
+
 function changementBoutonAuClic(){
     let button=document.querySelectorAll('.categorie');
-    button.addEventListener('click', ()=>{ //message d'erreur disant que ce n'est pas une fonction
+    button.addEventListener("mouseover", ()=>{ //message d'erreur disant que ce n'est pas une fonction
         button.setAttribute('color', 'white');
         button.setAttribute('background-color', '#1D6154');
        console.log()

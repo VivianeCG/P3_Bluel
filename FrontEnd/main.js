872d822f8bfb6c803e84343a89cac7ca86a756a8
VivianeCG <chenet.viviane@gmail.com>
2023-09-19 11:01:15 +0200

    les boutons et photos s'affichent sur le site

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,32 +1,56 @@
-const url = "http://localhost:5678/api/works"
-//fonction pour créer les balises figure dans le HTML
-
-window.onload=function creationFigure() {
-    let conteneurParent = document.getElementById("gallery");
-    let conteneurPhoto = document.createElement("figure");
-    let imageGallerie = document.createElement("img");
-    let legendePhoto = document.createElement("figcaption");
-    legendePhoto.innerText="legende photo";
-    conteneurParent.appendChild(conteneurPhoto);
-    conteneurPhoto.appendChild(imageGallerie);
-    conteneurPhoto.appendChild(legendePhoto);
-    imageGallerie.setAttribute("src","");
-    imageGallerie.setAttribute("alt","");
-    //return conteneurPhoto;
+//fonction pour créer les balises figure dans le HTML et récupérer les photos
+function creationFigure() {
+    const url = "http://localhost:5678/api/works"
+    /**pour récupérer les travaux depuis le Backend*/
+    fetch (url)
+    .then (function (response){
+        return response.json()
+    })
+    .then (function (data){
+        data.forEach(element => {
+                //pour créer les balises figures dans le html
+                let conteneurParent = document.getElementById("gallery");
+                let conteneurPhoto = document.createElement("figure");
+                let imageGallerie = document.createElement("img");
+                let legendePhoto = document.createElement("figcaption");
+                legendePhoto.innerText="legende photo";
+                conteneurParent.appendChild(conteneurPhoto);
+                conteneurPhoto.appendChild(imageGallerie);
+                conteneurPhoto.appendChild(legendePhoto);
+                imageGallerie.setAttribute("src","");
+                imageGallerie.setAttribute("alt","");
+                imageGallerie.src = element.imageUrl;
+                imageGallerie.alt = element.title;
+                legendePhoto.innerHTML = element.title; 
+            });
+        });
 }
-
-/**pour récupérer les travaux depuis le Backend*/
-fetch (url)
-.then (function (response){
-    return response.json()
-})
-.then (function (data){
-//pour créer les balises figures dans le html
-data.forEach(element => {
-        creationFigure()
-        imageGallerie.src = element.imageUrl;
-        imageGallerie.alt = element.title;
-        legendePhoto.innerHTML = element.title; 
-        console.log (element);
+creationFigure()
+//fontion pour créer les boutons de filtrage selon les catégories
+function creationFiltres() {
+    const urlCategories = "http://localhost:5678/api/categories"
+    fetch (urlCategories)
+    .then (function (response){
+        return response.json()
+    })
+    .then (function (data) {
+        data.forEach(element=> {
+            let divConteneurCategories = document.getElementById("filtres");
+            button = document.createElement("button");
+            button.setAttribute('class', 'categorie');
+            divConteneurCategories.appendChild(button);
+            button.innerHTML = element.name;
+            console.log(button);
+        })
     });
-});
+}
+creationFiltres()
+
+/*const photo = ["Booki","Celeste1", "Celeste2", "Celeste3", "Celeste4"]
+for (let button of photo) {
+    button = document.createElement("button");
+    button.setAttribute('class', 'categorie');
+    button.innerHTML = photo;
+    divConteneurCategories.appendChild(button);
+    console.log(button);
+};*/

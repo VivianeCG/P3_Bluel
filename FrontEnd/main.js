d8e312ac40091cf94bf4a97689bca7881011ff63
VivianeCG <chenet.viviane@gmail.com>
2023-09-25 21:43:48 +0200

    corrections en CSS

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,6 +1,6 @@
 //fonction pour créer les balises figure dans le HTML et récupérer les photos
 function creationFigure() {
-    const url = "http://localhost:5678/api/works"
+    const url = "http://localhost:5678/api/works";
     /**pour récupérer les travaux depuis le Backend*/
     fetch (url)
     .then (function (response){
@@ -28,7 +28,7 @@ function creationFigure() {
 creationFigure()
 //fontion pour créer les boutons de filtrage selon les catégories
 function creationFiltres() {
-    const urlCategories = "http://localhost:5678/api/categories"
+    const urlCategories = "http://localhost:5678/api/categories";
     fetch (urlCategories)
     .then (function (response){
         return response.json()
@@ -52,23 +52,18 @@ function creationFiltres() {
 }
 creationFiltres()
 
-function filtrageDesPhotos() {
-    const urlCategories = "http://localhost:5678/api/categories"
-    fetch (urlCategories)
-    .then (function (response){
-        return response.json()
-    })
-    .then (function (data) {
-        //idée créer un set pour éviter des doublons??
-        //const categorieId = categorieId.set("#categorie"+numeroId => categorieId.id); //problème de syntaxe à revoir
-        //ajouter un addEventListener pour que ça marche?
-        //un switch case serait plus adapté??
-        if () { //si l'id du bouton = l'id de la photo on l'affiche
-            console.log()
-            
-        } else {                // sinon on affiche toutes les photos
-            console.log()
-        }
-
-    });
+function {
+    const urlCategories = "http://localhost:5678/api/works";
+    let button = document.querySelector(".categorie");
+    for (let filtrage of button) {
+       fetch (urlCategories)
+        .then (function (response){
+            return response.json()
+        })
+        
+        .then (function (data) {
+        
+        }); 
+    }
+    
 };

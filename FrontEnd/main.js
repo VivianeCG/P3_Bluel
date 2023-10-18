0dc9470d1647e423c8d92081cd6ac3d3a227788b
VivianeCG <chenet.viviane@gmail.com>
2023-10-18 16:37:16 +0200

    indentation avec prettier

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,92 +1,88 @@
 //fonction pour créer les balises figure dans le HTML et récupérer les photos
 function creationFigure() {
-    const url = "http://localhost:5678/api/works";
-    //pour récupérer les travaux depuis le Backend
-    fetch (url)
-    .then (function (response){
-        return response.json()
+  const url = "http://localhost:5678/api/works";
+  //pour récupérer les travaux depuis le Backend
+  fetch(url)
+    .then(function (response) {
+      return response.json();
     })
-    .then (function (data){
-        data.forEach(element => {
-                //pour créer les balises figures dans le html
-                let conteneurParent = document.getElementById("gallery");
-                let conteneurPhoto = document.createElement("figure");
-                let imageGallerie = document.createElement("img");
-                let legendePhoto = document.createElement("figcaption");
-                legendePhoto.innerText="legende photo";
-                conteneurParent.appendChild(conteneurPhoto);
-                conteneurPhoto.appendChild(imageGallerie);
-                conteneurPhoto.appendChild(legendePhoto);
-                imageGallerie.setAttribute("src","");
-                imageGallerie.setAttribute("alt","");
-                imageGallerie.src = element.imageUrl;
-                imageGallerie.alt = element.title;
-                legendePhoto.innerHTML = element.title; 
-            });
-        });
+    .then(function (data) {
+      data.forEach((element) => {
+        //pour créer les balises figures dans le html
+        let conteneurParent = document.getElementById("gallery");
+        let conteneurPhoto = document.createElement("figure");
+        let imageGallerie = document.createElement("img");
+        let legendePhoto = document.createElement("figcaption");
+        legendePhoto.innerText = "legende photo";
+        conteneurParent.appendChild(conteneurPhoto);
+        conteneurPhoto.appendChild(imageGallerie);
+        conteneurPhoto.appendChild(legendePhoto);
+        imageGallerie.setAttribute("src", "");
+        imageGallerie.setAttribute("alt", "");
+        imageGallerie.src = element.imageUrl;
+        imageGallerie.alt = element.title;
+        legendePhoto.innerHTML = element.title;
+      });
+    });
 }
 //creationFigure()
 //fontion pour créer les boutons de filtrage selon les catégories
 function creationFiltres() {
-    const urlCategories = "http://localhost:5678/api/categories";
-    fetch (urlCategories)
-    .then (function (response){
-        return response.json()
+  const urlCategories = "http://localhost:5678/api/categories";
+  fetch(urlCategories)
+    .then(function (response) {
+      return response.json();
     })
-    .then (function (data) {
-        let numeroId = 1;
-        data.forEach(element=> {
-            //pour créer les boutons des filtres avec leurs noms et id(sauf "Tous")
-            let divConteneurCategories = document.getElementById("filtres");
-            let button = document.createElement("button");
-            button.setAttribute('class', 'categorie');
-            button.setAttribute('id', 'categorie'+numeroId);
-            numeroId++;
-            button.setAttribute('value', numeroId)
-            divConteneurCategories.appendChild(button);
-            button.innerHTML = element.name;
-            console.log(button);
-        })
-        
+    .then(function (data) {
+      let numeroId = 1;
+      data.forEach((element) => {
+        //pour créer les boutons des filtres avec leurs noms et id(sauf "Tous")
+        let divConteneurCategories = document.getElementById("filtres");
+        let button = document.createElement("button");
+        button.setAttribute("class", "categorie");
+        button.setAttribute("id", "categorie" + numeroId);
+        numeroId++;
+        button.setAttribute("value", numeroId);
+        divConteneurCategories.appendChild(button);
+        button.innerHTML = element.name;
+        console.log(button);
+      });
     });
 }
-creationFiltres()
+creationFiltres();
 
-function filtrageCategories(){
-    console.log("coucou");
-   const url = "http://localhost:5678/api/works";
-    let button = document.querySelectorAll(".categorie");
-        for (const filtrage of button) {
-            filtrage.addEventListener("click", ()=> {
+function filtrageCategories() {
+  console.log("coucou");
+  const url = "http://localhost:5678/api/works";
+  let button = document.querySelectorAll(".categorie");
+  for (const filtrage of button) {
+    filtrage.addEventListener("click", () => {
+      let conteneurParent = document.getElementById("gallery");
+      conteneurParent = "";
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          if (filtrage.value == 1) {
+            creationFigure();
+            console.log(filtrage.value);
+          } else {
+            console.log(filtrage.value);
+            for (let element of data) {
+              if (element.categoryId == filtrage.value) {
                 let conteneurParent = document.getElementById("gallery");
-                    conteneurParent="";
-                fetch (url)
-                .then (response=> response.json())
-                .then ((data) => {
-                    if (filtrage.value==1) {
-                        creationFigure();
-                        console.log(filtrage.value);
-                        } else {
-                            console.log(filtrage.value);
-                            for (let element of data){
-                                if (element.categoryId==filtrage.value){
-                                let conteneurParent = document.getElementById("gallery");
-                                let conteneurPhoto ="";
-                                    conteneurPhoto.innerHTML +=`
+                let conteneurPhoto = "";
+                conteneurPhoto.innerHTML += `
                                                         <figure>
                                                         <img src=${data[element].imageUrl} alt=${data[element].title}>
                                                         <figcaption>${data[element].title}</figcaption>
                                                         </figure>
                                                         `;
-                                conteneurParent.appendChild(conteneurPhoto);
-                                }
-                            }
-                        }
-                            
-                    });
-                });
-                    
-            };   
-};
+                conteneurParent.appendChild(conteneurPhoto);
+              }
+            }
+          }
+        });
+    });
+  }
+}
 filtrageCategories();
-
--- FrontEnd/pageConnexion.js
+++ FrontEnd/pageConnexion.js
@@ -1,26 +1,27 @@
-function listenerPourRecupererIdentifiants() { //connexion au compte
+//connexion au compte
 let remplissageIdentifiants = document.querySelector(".submit");
 //pour récupérer la valeur de chaque champs (e-mail et mot de passe)
 let identifiant = document.getElementById("email");
 let motDePasse = document.getElementById("mot-de-passe");
-    remplissageIdentifiants.addEventListener("click", async (event)=>{ 
-        event.preventDefault();
-    //pour stocker la valeur des 2 champs
-    let seConnecter = {
-        identifiant:identifiant.value,
-        password:motDePasse.value
-    };
-    console.log(seConnecter);
-});}
-
+remplissageIdentifiants.addEventListener("click", (event) => {
+  event.preventDefault();
+  //pour stocker la valeur des 2 champs
+  let seConnecter = {
+    identifiant: identifiant.value,
+    password: motDePasse.value,
+  };
+  console.log(seConnecter);
+});
 const identifiantConnexion = JSON.stringify(seConnecter);
 try {
-    const reponse = await fetch ("http://localhost:5678/api/users/login");
-    method:"POST",
-    headers = {"Accept": "application/json",
-                "Content-Type":"application/json"},
-    body = identifiantConnexion
-    console.log(reponse);
+  const reponse = fetch("http://urlConnexion:5678/api/users/login");
+  method: "POST",
+    (headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    }),
+    (body = identifiantConnexion);
+  console.log(reponse);
 } catch (error) {
-    alert ('l\'identifiant ou le mot de passe est erroné');
+  alert("l'identifiant ou le mot de passe est erroné");
 }

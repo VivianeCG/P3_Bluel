4e3e570e2b657b13b08d6ccd8dfca215c86c8dc6
VivianeCG <chenet.viviane@gmail.com>
2024-03-26 15:43:53 +0100

    début du filtrage ça fonctionne pour "Tous"

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -35,7 +35,7 @@ function createButton() {
       return response.json();
     })
     .then(function (data) {
-      let numberId = 1;
+      let numberId = 0;
       data.forEach((element) => {
         //pour créer les boutons des catégories avec leurs noms et id (sauf "Tous")
         let categoryContainerDiv = document.getElementById("filtres");
@@ -46,37 +46,32 @@ function createButton() {
         button.setAttribute("value", numberId);
         categoryContainerDiv.appendChild(button);
         button.innerHTML = element.name;
-        console.log(button);
+        //console.log(button);
       });
     });
 }
 createButton();
 
-//récupérer les boutons par id ou class ("categorie" + numberId) class="categorie"
-//mettre un eventlistener pour le clic sur le bouton
-//l'appel à l'API retourne un tableau, mettre le tableau dans un set?
-//boucler sur le set pour récupérer selon les categoryID  ou l'element.id:  pour les objets 1
-// 2 pour apparts et 3 pour hotels restos?
-// categorie0 attribuée à "tous"
-//condition si id categorie et id bouton match alors on affiche les travaux
-//par défaut ("tous") affiche tous les travaux
-//appeler la fonction createFigure si ça matche? 
-// attention faire appel à la fonction createButton dans cette fonction double les boutons
 function filterWorks(){
+  let filterButtons = document.querySelectorAll(".categorie");
   const url = "http://localhost:5678/api/works";
-  //pour récupérer les travaux depuis le Backend
-  fetch(url)
-    .then(function (response) {
-      return response.json();
+  for (const button of filterButtons) {
+    button.addEventListener("click", () => {
+      fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function(data){
+      })
+      console.log(button);
+      if (button.value==0) {
+        createFigure();
+        console.log("testTous");
+      } else if (button.value==1){
+        console.log("testAutresBoutons");
+      }
     })
-    .then(function(data){
-      const setOfWorks = new Set([data]);
-      const arrayOfWorks = Array.from(setOfWorks);
-      let worksButtonAll = document.getElementById("categorie0");
-      let worksButtonObjects = document.getElementById("categorie1");
-      let worksButtonFLats = document.getElementById("categorie2");
-      let worksButtonHotels = document.getElementById("categorie3");
-      
-    })
+    
+  }
 }
 filterWorks();

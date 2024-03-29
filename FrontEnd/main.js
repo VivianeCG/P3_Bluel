ee1c05080ee0b5f6bc4a6413065f6a58b9cfc986
VivianeCG <chenet.viviane@gmail.com>
2024-03-29 10:29:15 +0100

    fonctions filtrage terminées

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -35,43 +35,66 @@ function createButton() {
       return response.json();
     })
     .then(function (data) {
-      let numberId = 0;
+      let numberId = 1;
       data.forEach((element) => {
         //pour créer les boutons des catégories avec leurs noms et id (sauf "Tous")
         let categoryContainerDiv = document.getElementById("filtres");
         let button = document.createElement("button");
         button.setAttribute("class", "categorie");
         button.setAttribute("id", "categorie" + numberId);
-        numberId++;
         button.setAttribute("value", numberId);
+        numberId++;
         categoryContainerDiv.appendChild(button);
         button.innerHTML = element.name;
-        //console.log(button);
+        console.log(button);
       });
     });
 }
-createButton();
-
-function filterWorks(){
-  let filterButtons = document.querySelectorAll(".categorie");
+//createButton();
+function filterWorks() { //fonction pour filtrer les travaux "Tous" ou les autres catégories
   const url = "http://localhost:5678/api/works";
-  for (const button of filterButtons) {
-    button.addEventListener("click", () => {
+  document.getElementById("filtres").addEventListener("click", function(event) {
+    if (event.target && event.target.matches(".categorie")) {
+      let buttonValue = event.target.value;
       fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function(data){
-      })
-      console.log(button);
-      if (button.value==0) {
-        createFigure();
-        console.log("testTous");
-      } else if (button.value==1){
-        console.log("testAutresBoutons");
-      }
-    })
-    
-  }
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(event.target);
+          if (buttonValue == 0) {
+            const parentContainer = document.getElementById("gallery");
+            parentContainer.innerHTML = ""; //pour effacer le contenu précédent
+            createFigure();
+            console.log("testTous");
+          } else {
+            createFilteredFigure(data, buttonValue)
+            console.log("testAutresBoutons");
+
+          }
+        });
+    }
+  });
 }
+// Fonction pour créer les balises figure en filtrant les photos en fonction de l'ID de la catégorie
+function createFilteredFigure(data, buttonValue) {
+  const parentContainer = document.getElementById("gallery");
+  parentContainer.innerHTML = ""; // Effacer le contenu précédent
+  data.forEach((element) => {
+    if (element.categoryId == buttonValue) {
+      let photoContainer = document.createElement("figure");
+      let galleryImage = document.createElement("img");
+      let photoLegend = document.createElement("figcaption");
+      photoLegend.innerText = element.title;
+      parentContainer.appendChild(photoContainer);
+      photoContainer.appendChild(galleryImage);
+      photoContainer.appendChild(photoLegend);
+      galleryImage.setAttribute("src", element.imageUrl);
+      galleryImage.setAttribute("alt", element.title);
+    }
+  });
+}
+createButton();
 filterWorks();
+
+

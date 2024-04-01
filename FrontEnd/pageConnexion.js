1edd5cb8be6b19f0f111685cc503a536eb82dd6b
VivianeCG <chenet.viviane@gmail.com>
2024-04-01 21:35:24 +0200

    préparation connexion

--- FrontEnd/pageConnexion.js
+++ FrontEnd/pageConnexion.js
@@ -1,3 +1,57 @@
+// récupération e-mail
+let mailInput = document.getElementById("email");
+let userMail = mailInput.value;
+console.log(userMail);
+// vérifier que format e-mail est correct
+let checkValidUserMail = new RegExp('^[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+$');
+let resultUserMail = checkValidUserMail.test(userMail);
+console.log(resultUserMail);
+// récupération mdp
+let passwordInput = document.getElementById("mot-de-passe");
+let userPassword = passwordInput.value;
+console.log(userPassword);
+// vérifier que format du mdp est correct?
+//let checkValidUserPassword = new RegExp('^[a-z0-9._-#@{(^=+-*/%§?!)}]+$');
+//let resultUserPassword = checkValidUserPassword.test(userPassword);
+//console.log(resultUserPassword);
+// gestion de l'evt submit
+const form = document.querySelector("form");
+form.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  console.log("la page ne s'est pas rechargée")
+  if (userMail === "") {
+    console.log("userMail est vide");
+  } else {
+    console.log("userMail est rempli");
+  }
+  if (userPassword === "") {
+    console.log("userPassword est vide");
+  } else {
+    console.log("userPassword est rempli");
+  }
+});
+// prévenir user s'il y a une erreur
+// vérifier que user a le droit de se connecter,quel type de requête pour envoyer les infos?
+// stocker les infos de connexion
+// rediriger vers la page d'accueil en maintenant la connexion
+// stocker le token d'authentification pour pouvoir réaliser les envois et suppressions de travaux (= étape suivante)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //connexion au compte
 let remplissageIdentifiants = document.querySelector(".submit");
 //pour récupérer la valeur de chaque champs (e-mail et mot de passe)
@@ -25,3 +79,4 @@ try {
 } catch (error) {
   alert("l'identifiant ou le mot de passe est erroné");
 }
+*/

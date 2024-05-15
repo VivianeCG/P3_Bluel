7ad15acfa71e414bed408717c396b77d292507e2
VivianeCG <chenet.viviane@gmail.com>
2024-05-15 16:40:13 +0200

    la fonction deactivateEditMode fonctionne, modification de la fonction defaultIndexPage pour permettre l'exportation de certaines fonctions

--- FrontEnd/JavaScript/editPage.js
+++ FrontEnd/JavaScript/editPage.js
@@ -1,22 +1,24 @@
-import { defaultIndexPage } from "./main.js";
+import { defaultIndexPage, createCard, createFigure, createButton } from "./main.js";
 //fonction pour lancer l'apparence du mode édition
 export function activateEditMode() {
         document.getElementById('edit').style.display='flex';
         document.getElementById('edit-button').style.display='flex';
         document.getElementById('login').style.display = 'none';
         document.getElementById('logout').style.display = 'flex';
-
+        console.log("fonction activateEditMode");
   }
 //activateEditMode();
 //fonction pour le retour à l'apparence normale au clic sur logout
 export function deactivateEditMode() {
-   document.getElementById('logout').addEventListener('click', ()=>{
+   document.getElementById('logout').addEventListener('click', (event)=>{
+    event.preventDefault();
     document.getElementById('edit').style.display='none';
     document.getElementById('edit-button').style.display='none';
     document.getElementById('logout').style.display = 'none';
     document.getElementById('login').style.display = 'flex';
     //ça ne marche pas mieux si j'utilise la fonction importée
     //ni si j'utilise "submit" ou "change" à la place de "click"
+    console.log("fonction deactivateEditMode");
 }) 
 }
 
--- FrontEnd/JavaScript/main.js
+++ FrontEnd/JavaScript/main.js
@@ -1,105 +1,110 @@
 //fonctions importées
 import { activateEditMode, deactivateEditMode } from "./editPage.js";
+// fonction pour créer les cadres des photos
+export function createCard(element) {
+  let photoContainer = document.createElement("figure");
+  let galleryImage = document.createElement("img");
+  let photoLegend = document.createElement("figcaption");
+  let parentContainer = document.getElementById("gallery");
+  let binIcon = document.createElement("i");
+  binIcon.classList.add("fa-solid", "fa-trash-can", "bin");
+  photoLegend.innerText = element.title;
+  parentContainer.appendChild(photoContainer);
+  photoContainer.appendChild(galleryImage);
+  photoContainer.appendChild(photoLegend);
+  photoContainer.appendChild(binIcon);
+  galleryImage.setAttribute("src", element.imageUrl);
+  galleryImage.setAttribute("alt", element.title);
+}
+
+//fonction pour récupérer les photos lors de l'affichage de la page
+export function createFigure() {
+  const url = "http://localhost:5678/api/works";
+  //pour récupérer les travaux depuis le Backend
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      data.forEach((element) => {
+        //pour créer les balises figures dans le html
+        createCard(element);
+      });
+    });
+}
+createFigure();
+
+//fonction pour créer les boutons des différentes catégories
+export function createButton() {
+  const urlCategories = "http://localhost:5678/api/categories";
+  fetch(urlCategories)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let numberId = 1;
+      data.forEach((element) => {
+        //pour créer les boutons des catégories avec leurs noms et id (sauf "Tous")
+        let categoryContainerDiv = document.getElementById("filtres");
+        let button = document.createElement("button");
+        button.setAttribute("class", "categorie");
+        button.setAttribute("id", "categorie" + numberId);
+        button.setAttribute("value", numberId);
+        numberId++;
+        categoryContainerDiv.appendChild(button);
+        button.innerHTML = element.name;
+        console.log(button);
+      });
+    });
+}
+createButton();
+//fonction pour filtrer les travaux "Tous" ou les autres catégories
+function filterWorks() {
+  const url = "http://localhost:5678/api/works";
+  document
+    .getElementById("filtres")
+    .addEventListener("click", function (event) {
+      if (event.target && event.target.matches(".categorie")) {
+        let buttonValue = event.target.value;
+        fetch(url)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(event.target);
+            if (buttonValue == 0) {
+              const parentContainer = document.getElementById("gallery");
+              parentContainer.innerHTML = ""; //pour effacer le contenu précédent
+              createFigure();
+              console.log("testTous");
+            } else {
+              createFilteredFigure(data, buttonValue);
+              console.log("testAutresBoutons");
+            }
+          });
+      }
+    });
+}
+// Fonction pour créer les balises figure en filtrant les photos en fonction de l'ID de la catégorie
+function createFilteredFigure(data, buttonValue) {
+  const parentContainer = document.getElementById("gallery");
+  parentContainer.innerHTML = ""; // Effacer le contenu précédent
+  data.forEach((element) => {
+    if (element.categoryId == buttonValue) {
+      createCard(element);
+    }
+  });
+}
+//createButton();
+filterWorks();
+
 //fonction pour faire apparaître la page par défaut
 export function defaultIndexPage() {
-  // fonction pour créer les cadres des photos
-  function createCard(element) {
-    let photoContainer = document.createElement("figure");
-    let galleryImage = document.createElement("img");
-    let photoLegend = document.createElement("figcaption");
-    let parentContainer = document.getElementById("gallery");
-    let binIcon = document.createElement('i');
-    binIcon.classList.add('fa-solid','fa-trash-can','bin');
-    photoLegend.innerText = element.title;
-    parentContainer.appendChild(photoContainer);
-    photoContainer.appendChild(galleryImage);
-    photoContainer.appendChild(photoLegend);
-    photoContainer.appendChild(binIcon);
-    galleryImage.setAttribute("src", element.imageUrl);
-    galleryImage.setAttribute("alt", element.title);
-  }
-  
-  //fonction pour récupérer les photos lors de l'affichage de la page
-  function createFigure() {
-    const url = "http://localhost:5678/api/works";
-    //pour récupérer les travaux depuis le Backend
-    fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        data.forEach((element) => {
-          //pour créer les balises figures dans le html
-          createCard(element);
-        });
-      });
-  }
+  createCard();
   createFigure();
-  
-  //fonction pour créer les boutons des différentes catégories
-  function createButton() {
-    const urlCategories = "http://localhost:5678/api/categories";
-    fetch(urlCategories)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        let numberId = 1;
-        data.forEach((element) => {
-          //pour créer les boutons des catégories avec leurs noms et id (sauf "Tous")
-          let categoryContainerDiv = document.getElementById("filtres");
-          let button = document.createElement("button");
-          button.setAttribute("class", "categorie");
-          button.setAttribute("id", "categorie" + numberId);
-          button.setAttribute("value", numberId);
-          numberId++;
-          categoryContainerDiv.appendChild(button);
-          button.innerHTML = element.name;
-          console.log(button);
-        });
-      });
-  }
   createButton();
-  //fonction pour filtrer les travaux "Tous" ou les autres catégories
-  function filterWorks() {
-     const url = "http://localhost:5678/api/works";
-   document.getElementById("filtres").addEventListener("click", function(event) {
-     if (event.target && event.target.matches(".categorie")) {
-       let buttonValue = event.target.value;
-       fetch(url)
-         .then(function(response) {
-           return response.json();
-         })
-         .then(function(data) {
-           console.log(event.target);
-           if (buttonValue == 0) {
-             const parentContainer = document.getElementById("gallery");
-             parentContainer.innerHTML = ""; //pour effacer le contenu précédent
-             createFigure();
-             console.log("testTous");
-           } else {
-             createFilteredFigure(data, buttonValue)
-             console.log("testAutresBoutons");
-  
-           }
-         });
-     }
-   });
-  }
-  // Fonction pour créer les balises figure en filtrant les photos en fonction de l'ID de la catégorie
-  function createFilteredFigure(data, buttonValue) {
-   const parentContainer = document.getElementById("gallery");
-   parentContainer.innerHTML = ""; // Effacer le contenu précédent
-   data.forEach((element) => {
-     if (element.categoryId == buttonValue) {
-       createCard(element);
-     }
-   });
-  }
-  //createButton();
   filterWorks();
 }
-defaultIndexPage();
 
 activateEditMode();
 
